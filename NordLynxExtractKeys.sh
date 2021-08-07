177f04dcfa4f0be02441d6de2de8141a41141412
wifiuk <wifiuk@users.noreply.github.com>
2021-08-07 19:15:11 +0100

    Update NordLynxExtractKeys.sh

--- NordLynxExtractKeys.sh
+++ NordLynxExtractKeys.sh
@@ -11,23 +11,35 @@ echo "== Connecting to NordVPN Server =="
 nordvpn c $1 > /dev/null 2>&1
 echo "== Now extracting Wireguard Configurtion infomation =="
 current_server=$(nordvpn status | grep "Current server:" |awk '{print $3 "\t" $4}')
-interface=$(sudo wg show | grep "interface")
 public_key=$(sudo wg show | grep "public key")
-listening_port=$(sudo wg show | grep "listening port")
-peer=$(sudo wg show | grep "peer")
-endpoint=$(sudo wg show | grep "endpoint")
-keepalive=$(sudo wg show | grep "persistent keepalive")
+listening_port=$(sudo wg showconf nordlynx | grep "ListenPort")
+private_key=$(sudo wg showconf nordlynx | grep "PrivateKey")
+allowed_ip=$(sudo wg showconf nordlynx | grep "AllowedIPs")
+endpoint=$(sudo wg showconf nordlynx | grep "Endpoint")
+keepalive=$(sudo wg showconf nordlynx | grep "PersistentKeepalive")
 echo "== Saving information to file $current_server =="
 echo ""
 echo ""
-echo $current_server | tee $current_server
-echo $interface | tee -a $current_server
-echo $public_key | tee -a $current_server
-echo $listening_port | tee -a $current_server
-echo $peer | tee -a $current_server
-echo $endpoint | tee -a $current_server
-echo $keepalive | tee -a $current_server
+echo "Server Name = $current_server " | tee $current_server
+echo "[Interface]" | tee -a $current_server
 echo ""
-echo""
+echo $listening_port | tee -a $current_server
+echo ""
+echo $private_key | tee -a $current_server
+echo ""
+echo ""
+echo ""
+echo "[Peer]" | tee -a $current_server
+echo ""
+echo $public_key | tee -a $current_server
+echo ""
+echo $allowed_ip | tee -a $current_server
+echo ""
+echo $endpoint | tee -a $current_server
+echo ""
+echo $keepalive | tee -a $current_server
+
+echo ""
+echo ""
 echo "== Disconnecting from NordVPN =="
 sudo nordvpn d > /dev/null 2>&1
