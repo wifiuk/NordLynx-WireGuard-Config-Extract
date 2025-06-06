bcf3d06787978965e01ab55ef2ec8d99ede68d5e
FineSu <brecciasf@users.noreply.github.com>
2025-06-06 11:38:00 +0800

    Updated NordLynxExtractKeys.sh to detect prior connected status.
    Executes commands once and captures output. Then uses that output going forward.
    
    Checks to see if you are already connected to nordvpn and in that case just extracts the information of the current connection.
    
    Output format easier to change going forward.
    
    Added 'Server Name' , 'Server City' and 'Server Country' to the output.

--- NordLynxExtractKeys.sh
+++ NordLynxExtractKeys.sh
@@ -1,45 +1,96 @@
 #!/bin/bash
+echo -e "\nCode is from : https://github.com/wifiuk/NordLynx-WireGuard-Config-Extract\n\n"
 echo "NordVPN needs to be installed and your credentials added for this to work."
 echo "If you have not already installed wireguard, please do so before running this tool."
 echo "sudo apt install wireguard -y"
-echo "usage example: sudo bash NordLynxExtractKeys.sh uk"
+echo "usage example: sudo bash $(basename $0) uk"
 echo ""
 echo ""
-echo "== Setting NordVPN to use NordLynx =="
-nordvpn set technology NordLynx > /dev/null 2>&1
-echo "== Connecting to NordVPN Server =="
-nordvpn c $1 > /dev/null 2>&1
+
+# location to write files to.
+file_save_dir="/tmp"
+# flag to indicate that we 'force' connected to nordvpn.
+flag_system_triggered_connection=0 
+# Check if 'nordvpnd' daemon is running
+pid_nordvpn=$(pgrep -u root nordvpnd)
+if [[ $? -eq 0 ]]; then
+    # If it's running...check it's status.
+    status_=$(nordvpn status | awk '/Status:\s+.*/ {print $NF}')
+    if [[ "$status_" == "Connected" ]]; then
+        # if it's connected to a vpn just show that info.
+        flag_system_triggered_connection=0
+        echo "== NordVPN is Already Running =="
+
+    elif [[ "$status_" == "Disconnected" ]]; then
+        # if it's not connected to a vpn first connect then show info.
+        flag_system_triggered_connection=1
+
+        echo "== NordVPN is Disconnected =="
+
+        echo "== Setting NordVPN to use NordLynx =="
+        nordvpn set technology NordLynx > /dev/null 2>&1
+
+        echo "== Connecting to NordVPN Server =="
+        nordvpn c $1 > /dev/null 2>&1
+    fi
+
+else
+    echo "============================================="
+    echo "ERROR: NordVPN Daemon is Not Running"
+    echo "run: 'sudo systemctl restart nordvpnd'"
+    echo "============================================="
+    exit -1
+fi
+
+
 echo "== Now extracting Wireguard Configurtion infomation =="
-current_server=$(nordvpn status | grep "Hostname:" |awk '{print $2}')
-public_key=$(sudo wg show | grep "public key")
-listening_port=$(sudo wg showconf nordlynx | grep "ListenPort")
-private_key=$(sudo wg showconf nordlynx | grep "PrivateKey")
-allowed_ip=$(sudo wg showconf nordlynx | grep "AllowedIPs")
-endpoint=$(sudo wg showconf nordlynx | grep "Endpoint")
-keepalive=$(sudo wg showconf nordlynx | grep "PersistentKeepalive")
-echo "== Saving information to file $current_server =="
-echo ""
-echo ""
-echo "Server Name = $current_server " | tee $current_server
-echo "[Interface]" | tee -a $current_server
-echo ""
-echo $listening_port | tee -a $current_server
-echo ""
-echo $private_key | tee -a $current_server
-echo ""
-echo ""
-echo ""
-echo "[Peer]" | tee -a $current_server
-echo ""
-echo $public_key | tee -a $current_server
-echo ""
-echo $allowed_ip | tee -a $current_server
-echo ""
-echo $endpoint | tee -a $current_server
-echo ""
-echo $keepalive | tee -a $current_server
+
+nordvpn_status=$(nordvpn status)
+current_server_name=$(awk '/Server:\s+/ {print $2 $3 }' <<< "$nordvpn_status")
+current_server_city=$(awk '/City:/ {print $NF}' <<< "$nordvpn_status")
+current_server_country=$(awk '/Country:/ {print $NF}' <<< "$nordvpn_status")
+current_server=$(awk '/Hostname:/ {print $NF}' <<< "$nordvpn_status")
+
+wireguard_status=$(sudo wg show)
+public_key=$(awk '/public key:/ {print $NF}' <<< "$wireguard_status")
+
+inet=$(ip addr show dev nordlynx | awk '/inet/ {print $2}')
+
+wg_conf=$(sudo wg showconf nordlynx)
+listening_port=$(awk '/ListenPort/ {print $NF}' <<< "$wg_conf")
+private_key=$(awk '/PrivateKey/ {print $NF}' <<< "$wg_conf")
+allowed_ip=$(awk '/AllowedIPs/ {print $NF}' <<< "$wg_conf")
+endpoint=$(awk '/Endpoint/ {print $NF}' <<< "$wg_conf")
+keepalive=$(awk '/PersistentKeepalive/ {print $NF}' <<< "$wg_conf")
+
+# a copy of the output is sent here.
+save_file="${file_save_dir}/${current_server}.txt"
+cat << EOF | tee "$save_file"
+Server Hostname = ${current_server}
+Server Name = ${current_server_name}
+Server City = ${current_server_city}
+Server Country = ${current_server_country}
+
+[Interface]
+ListenPort: ${listening_port}
+PrivateKey: ${private_key}
+
+[Peer]
+PublicKey: ${public_key}
+AllowedIPs: ${allowed_ip}
+Endpoint: ${endpoint}
+keepalive: ${keepalive}
+
+[Address]
+inet: ${inet}
+EOF
 
 echo ""
 echo ""
-echo "== Disconnecting from NordVPN =="
-sudo nordvpn d > /dev/null 2>&1
+echo "== File saved : $save_file =="
+
+
+if [[ "$flag_system_triggered_connection" == "1" ]]; then
+    echo "== Disconnecting from NordVPN =="
+    sudo nordvpn d > /dev/null 2>&1
+fi
